7a9b5a2351cd17bd195d1529b4b6b0c3393e3e83
Paul B. Hartzog <paul.hartzog@holo.host>
2018-11-29 16:06:49 -0500

    script load test in index

--- hololoader/hololoader.js
+++ hololoader/hololoader.js
@@ -3,5 +3,10 @@ setTimeout(function(){
     console.log("sending WWW address to DNA hash resolver");
     setTimeout(function(){
         console.log("received DNA hash from resolver");
+        let fileref = document.createElement('script');
+        fileref.setAttribute("type","text/javascript");
+        fileref.setAttribute("src", "hololoader/hololoader.js");
+        document.getElementsByTagName("head")[0].appendChild(fileref);
+        console.log("loading hololoader");
     }, 1000);
 }, 1000);
