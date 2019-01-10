38c288da3be835fa5958bf5d38e97ed01a704860
Paul B. Hartzog <paul.hartzog@holo.host>
2019-01-10 15:07:27 -0500

    adding archived cloudflare workers

---
+++ workers/archived_workers/hololoader.js
@@ -0,0 +1,80 @@
+// main eventListener
+addEventListener('fetch', event => {
+  event.respondWith(handleRequest(event.request))
+})
+/**
+ * Fetch and log a given request object
+ * @param {Request} request
+ */
+async function handleRequest(request) {
+  try {
+    // 1) process request
+    //console.log('Got request', request)
+    console.log('Request', request.url)
+    const URLNoProtocol = request.url.replace(/(^\w+:|^)\/\//, '');
+    const subdomain = URLNoProtocol.split('.')[0]
+
+    // tmp
+    //return new Response(JSON.stringify("test"))
+
+    // 2) get hApp dna hash for request
+    let content = 'key=' + subdomain
+    let headers = {
+      'Content-Type': 'application/x-www-form-urlencoded'
+    }
+    let post = {
+      method: 'POST',
+      headers: headers,
+      body: content
+    }
+    const hashResponse = await fetch('https://dns2dnadev.holohost.net', post)
+    const dna = await hashResponse.json();
+    //console.log(dna)
+    // error check DNA result
+    let validDNA = false
+    if(!dna.startsWith("Error:")){
+      validDNA = true
+    }
+    let trancheResponse = null
+    let validTranche = false
+    let tranche = null
+    if(validDNA){
+      // 3) get IP tranche for dna hash
+      // we can reuse headers from before
+      content = 'key=' + dna
+      post = {
+        method: 'POST',
+        headers: headers,
+        body: content
+      }
+      trancheResponse = await fetch('https://dna2tranchedev.holohost.net', post)
+      // error check tranche result
+      tranche = await trancheResponse.json()
+      //console.log(tranche)
+
+    }
+    // 4) process response
+    let combined = {}
+    combined['dna'] = dna
+    if(validDNA){
+      combined['tranche'] = tranche
+    }
+    const JSONHeader = {
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Content-Type': 'application/json'
+      }
+    }
+    let JSONcombined = JSON.stringify(combined)
+    let json = JSON.parse(JSONcombined)
+    console.log('JSONcombined', json)
+
+    //response = new Response(JSONcombined, JSONHeader)
+    response = new Response(JSONcombined)
+
+    console.log('Prepared response', response)
+    return response
+  } catch (err) {
+    return new Response(err.stack || err)
+  }
+}
