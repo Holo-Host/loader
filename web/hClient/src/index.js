05688ff981c2b5ad6e5eef43603258c72b1dc024
Willem Olding <willemolding@gmail.com>
2019-02-22 17:22:19 +1100

    remove submodule to add back in a different locatioon

--- web/hClient/src/index.js
+++ web/hClient/src/index.js
@@ -3,11 +3,11 @@ require('babel-polyfill');
  * hClient.js
  * An API compatible drop-in for hc-web-client. This is inserted by the loader on page load and overrideWebClient() called
  * 
- * Exports a single function, overrideWebClient, that will instantly holo-ify a holochain app.
+ * Exports a single function, init(), that will instantly holo-ify a holochain app.
  * It adds the additional functionality of
  *     - Connecting to the correct holo-port
  *     - Signing all calls with the agents private key
- *     - Intercepting 401 responses to display a login screen
+ *     - Intercepting unauthorized responses to display a login screen
  */
 
 module.exports = hClient = (function(){
